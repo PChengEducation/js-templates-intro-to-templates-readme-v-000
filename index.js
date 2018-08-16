5cea0eea0dd809eb35031e61c2222b11b389097f
Pan Cheng <pchengeducation@hotmail.com>
2018-08-16 16:09:08 +0000

    done intro to templates

--- index.js
+++ index.js
@@ -1,22 +1,23 @@
-function addCommnet(){
-  var bodyText = "This research is bold and important!";
-  var commenter = "Definitely not a woodchuck looking to eat the wood";
+function addComment() {
+  event.preventDefault();
+  var bodyText = document.getElementById("commentText").value;
+  var commenter = document.getElementById("commenterName").value;
   var commentBody = createCommentBody(bodyText);
   var commenterLabel = createCommenterLabel(commenter);
   postNewComment(commentBody, commenterLabel);
 }
-
-function createCommentBody(comment){
+ 
+function createCommentBody(comment) {
   var bodyDiv = document.createElement("div");
   var bodyPara = document.createElement("p");
   bodyPara.innerHTML = comment;
   bodyDiv.appendChild(bodyPara);
   return bodyDiv;
 }
-
-function createCommenterLabel(commenter){
+ 
+function createCommenterLabel(commenter) {
   var commenterDiv = document.createElement("div");
-  var commenterLable = document.createElement("p");
+  var commenterLabel = document.createElement("p");
   commenterLabel.innerHTML = "posted by:&nbsp;";
   var commenterName = document.createElement("span");
   commenterName.innerHTML = commenter;
@@ -24,11 +25,11 @@ function createCommenterLabel(commenter){
   commenterDiv.appendChild(commenterLabel);
   return commenterLabel;
 }
-
-function postNewComment(body, commenter){
-  var commentersDiv = document.getElementById("comments");
+ 
+function postNewComment(body, commenter) {
+  var commentsDiv = document.getElementById("comments");
   var newCommentDiv = document.createElement("div");
   newCommentDiv.appendChild(body);
   newCommentDiv.appendChild(commenter);
-  commentsDiv.appenChild(newCommentDiv);
+  commentsDiv.appendChild(newCommentDiv);
 }
